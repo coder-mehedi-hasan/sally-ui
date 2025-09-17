b06a49bbbdd70098e52fcafd8829ea209ccd5b0b
coder-mehedi-hasan <mh8399807@gmail.com>
2025-09-17 13:40:49 +0600

    up

--- server/index.js
+++ server/index.js
@@ -193,6 +193,8 @@ wss.on('connection', (ws) => {
   ws.on('close', () => sockets.delete(ws));
 });
 
-server.listen(PORT, () => {
-  console.log(`[sally-ui] listening on http://localhost:${PORT}`);
-});
+// server.listen(PORT, () => {
+//   console.log(`[sally-ui] listening on http://localhost:${PORT}`);
+// });
+
+export default app;
