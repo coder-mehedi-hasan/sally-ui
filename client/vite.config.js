cab17b70bad3b4f988270938d7d79321a3d6d769
coder-mehedi-hasan <mh8399807@gmail.com>
2025-09-30 16:28:26 +0600

    u[

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,17 +4,17 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  },
   server: {
     port: Number(process.env.PORT || 5173),
     proxy: {
       '/api': {
         target: process.env.API_BASE || 'https://api.quarkshub.com',
-        changeOrigin: true,
+        changeOrigin: true
       }
     }
-  },
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true
   }
 })
